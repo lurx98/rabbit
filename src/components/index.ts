3fa32bce75a85e9e40d15d6ecab7d103f0f80d76
lurx98 <lurx98@gmail.com>
2022-09-03 20:09:19 +0800

    查看更多组件

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,11 @@
 import skeleton from '@/components/skeleton/index.vue'
 import carousel from '@/components/carousel/index.vue'
+import more from '@/components/more/index.vue'
 import { App } from 'vue'
 export default {
   install(app: App) {
     app.component(skeleton.name, skeleton)
     app.component(carousel.name, carousel)
+    app.component(more.name, more)
   }
 }
