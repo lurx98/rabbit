034391e2f101fd0b5683a92fd7351909c11a1591
lurx98 <lurx98@gmail.com>
2022-09-03 21:58:11 +0800

    优化新鲜好物和人气推荐

--- src/views/home/components/HomeHot.vue
+++ src/views/home/components/HomeHot.vue
@@ -11,7 +11,7 @@ const target = useLazyData(() => {
 
 <template>
   <HomePanel ref="target" title="人气推荐" sub-title="人气爆款 不容错过">
-    <ul ref="panel" class="goods-list">
+    <ul ref="panel" class="goods-list" v-if="home.getHotList.length > 0">
       <li v-for="item in home.hotGoodList" :key="item.id">
         <RouterLink to="/">
           <img v-lazy="item.picture" alt="" />
@@ -20,6 +20,13 @@ const target = useLazyData(() => {
         </RouterLink>
       </li>
     </ul>
+    <div class="home-skeleton" v-else>
+      <div class="item" v-for="i in 4" :key="i" :style="{ backgroundColor: '#f0f9f4' }">
+        <XtxSkeleton bg="#e4e4e4" :width="306" :height="306" />
+        <XtxSkeleton bg="#e4e4e4" :width="160" :height="24" />
+        <XtxSkeleton bg="#e4e4e4" :width="120" :height="24" />
+      </div>
+    </div>
   </HomePanel>
 </template>
 
@@ -44,6 +51,19 @@ const target = useLazyData(() => {
     .desc {
       color: #999;
       font-size: 18px;
+    }
+  }
+  .home-skeleton {
+    width: 1240px;
+    height: 406px;
+    display: flex;
+    justify-content: space-between;
+    .item {
+      width: 306px;
+      .xtx-skeleton ~ .xtx-skeleton {
+        display: block;
+        margin: 16px auto 0;
+      }
     }
   }
 }
--- src/views/home/components/HomeNew.vue
+++ src/views/home/components/HomeNew.vue
@@ -15,7 +15,7 @@ const target = useLazyData(() => {
     <HomePanel ref="target" title="新鲜好物" sub-title="新鲜出炉 品质靠谱">
       <template #right><XtxMore path="/" /></template>
       <!-- 面板内容 -->
-      <ul class="goods-list">
+      <ul class="goods-list" v-if="home.goodsList.length > 0">
         <li v-for="item in home.goodsList" :key="item.id">
           <RouterLink to="/">
             <img v-lazy="item.picture" alt="" />
@@ -24,6 +24,13 @@ const target = useLazyData(() => {
           </RouterLink>
         </li>
       </ul>
+      <div class="home-skeleton" v-else>
+        <div class="item" v-for="i in 4" :key="i" :style="{ backgroundColor: '#f0f9f4' }">
+          <XtxSkeleton bg="#e4e4e4" :width="306" :height="306" />
+          <XtxSkeleton bg="#e4e4e4" :width="160" :height="24" />
+          <XtxSkeleton bg="#e4e4e4" :width="120" :height="24" />
+        </div>
+      </div>
     </HomePanel>
   </div>
 </template>
@@ -49,6 +56,19 @@ const target = useLazyData(() => {
     }
     .price {
       color: @priceColor;
+    }
+  }
+}
+.home-skeleton {
+  width: 1240px;
+  height: 406px;
+  display: flex;
+  justify-content: space-between;
+  .item {
+    width: 306px;
+    .xtx-skeleton ~ .xtx-skeleton {
+      display: block;
+      margin: 16px auto 0;
     }
   }
 }
