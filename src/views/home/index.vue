1d9940d5def4f9478b78fe443561bebaccefab1d
lurx98 <lurx98@gmail.com>
2022-09-03 20:27:39 +0800

    通用面板组件

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,6 +2,7 @@
 import { reactive, ref } from 'vue'
 import HomeCategory from './components/HomeCategory.vue'
 import HomeBanner from './components/HomeBanner.vue'
+import HomePanel from './components/HomePanel.vue'
 </script>
 
 <template>
@@ -12,6 +13,11 @@ import HomeBanner from './components/HomeBanner.vue'
         <HomeCategory />
         <!-- banner轮播图 -->
         <HomeBanner />
+        <HomePanel title="新鲜好物" sub-title="新鲜出炉 品质靠谱">
+          <template #right>
+            <XtxMore to="/" />
+          </template>
+        </HomePanel>
       </div>
     </div>
   </div>
